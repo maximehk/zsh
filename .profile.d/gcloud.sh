c7545e5c1e24319bd66ce869e6be0653b48c6f87
Maxime HK <na>
2017-02-25 16:06:29 +0000

    Updated gcloud.sh

--- .profile.d/gcloud.sh
+++ .profile.d/gcloud.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '$HOME/.local/google-cloud-sdk/path.zsh.inc' ]; then source '$HOME/.local/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f "$HOME/.local/google-cloud-sdk/path.zsh.inc" ]; then source "$HOME/.local/google-cloud-sdk/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '$HOME/.local/google-cloud-sdk/completion.zsh.inc' ]; then source '$HOME/.local/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f "$HOME/.local/google-cloud-sdk/completion.zsh.inc" ]; then source "$HOME/.local/google-cloud-sdk/completion.zsh.inc"; fi
