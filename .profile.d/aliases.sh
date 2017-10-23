000f7bcc626b1a7b1da60088069e2dfed6d758fd
Maxime Hacker <maximeh@google.com>
2017-10-23 15:19:45 +0200

    Updated `date` functions

--- .profile.d/aliases.sh
+++ .profile.d/aliases.sh
@@ -2,23 +2,25 @@
 
 # some more ls aliases
 alias ll='ls -lF --color=auto'
+alias l='ls -lFrt --color=auto'
 alias la='ls -A'
-alias l='ls -CF'
 
 alias grep="grep --color"
 alias ..="cd .."
 
 # Display time in a different timezone
-alias syd='TZ=Australia/Sydney date'
-alias tok='TZ=Asia/Tokyo date'
-alias pdt='TZ=America/Los_Angeles date'
-alias pst='TZ=America/Los_Angeles date'
-alias nyt='TZ=America/New_York date'
-alias edt='TZ=America/New_York date'
-alias d='TZ=Europe/Paris date'
+function syd  { TZ="Australia/Sydney" date $@ +'%F %T %Z' ; }
+function tok  { TZ="Asia/Tokyo" date $@ +'%F %T %Z' ; }
+function pdt  { TZ="America/Los_Angeles" date $@ +'%F %T %Z' ; }
+function pst  { TZ="America/Los_Angeles" date $@ +'%F %T %Z' ; }
+function nyt  { TZ="America/New_York" date $@ +'%F %T %Z' ; }
+function edt  { TZ="America/New_York" date $@ +'%F %T %Z' ; }
+function d  { TZ="Europe/Paris" date $@ +'%F %T %Z' ; }
 
 # Misc
 alias xclip="xclip -selection c"  # copy stdin to clipboard
 
 # Alias SublimeText with `s` if it is in the path
 sublime_path=$(which subl) 2> /dev/null && alias s="${sublime_path}"
+
+
