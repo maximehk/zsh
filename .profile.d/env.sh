8fd607ee57700e2259d8caf7630c31b51cb6949e
Maxime Hacker <maximeh@google.com>
2018-11-13 10:19:20 +0100

    added noconda

--- .profile.d/env.sh
+++ .profile.d/env.sh
@@ -3,3 +3,22 @@
 _local_bin="$HOME/.local/bin"
 mkdir -p ${_local_bin}
 export PATH="${_local_bin}:$PATH"
+
+path_remove_matching() {
+  pattern="$1"
+  export PATH="$(echo $PATH | perl -lne 'BEGIN{$/=":" ; $\=":"; $pattern=shift} ; if (!/$pattern/ and $c{$_}++==0) {print}' -- $pattern)"
+}
+
+noconda() { path_remove_matching "anaconda" }
+
+path_show() {
+  echo "$PATH" | tr ':' '\n'
+}
+
+
+# GOLANG
+if [[ -d "$HOME/.local/go/bin" && -d "$HOME/go" ]] ; then
+  export GOROOT="$HOME/.local/go"
+  export PATH="$PATH:$GOROOT/bin"
+  export GOPATH="$HOME/go"
+fi
